277c2142b3dfd0ee1d73d904cbaa07e148b5cc95
United-ka <zurajaxveladze1001@gmail.com>
2025-01-06 17:40:26 +0400

    commit

---
+++ java.js
@@ -0,0 +1,5 @@
+// You can add interactive features with JavaScript
+document.addEventListener('DOMContentLoaded', function() {
+    // Example: alert when page loads
+    alert('კარგად მოედით რეალ მადრიდის ფანკლუბში!');
+});
